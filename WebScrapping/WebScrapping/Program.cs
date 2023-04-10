9dd92b6d3eb840524b573014a1de7a6143f960ef
Filipe Cerejo <44866025+FilipeCerejo@users.noreply.github.com>
2023-04-10 13:59:55 +0100

    Add files via upload

---
+++ WebScrapping/WebScrapping/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Aspose.Html;
+using Aspose.Html.Dom;
+using WebScrapping;
+
+string site = "https://www.letras.mus.br";
+string lyricsUrl = "/novo-hinario-adventista";
+List<Song> songList = new();
+
+//Url and Name
+using (var lyricsDocument = new HTMLDocument($"{site}{lyricsUrl}"))
+{
+    var songNode = lyricsDocument.Evaluate("//*[@class='song-name']", lyricsDocument, null, Aspose.Html.Dom.XPath.XPathResultType.Any, null);
+    int i = 0;
+    for (Node node; (node = songNode.IterateNext()) != null;)
+    {
+        songList.Add(new Song()
+        {
+            Name = node.TextContent,
+            Url = $"{site}{node.Attributes["href"].Value}"
+        });
+        if (++i == 10) break;
+    }
+}
+
+//Lyrics
+foreach(Song song in songList)
+{
+    try
+    {
+        var songDocument = new HTMLDocument(song.Url);
+
+        var songLyricsNode = songDocument.Evaluate("//*[@class='cnt-letra']//p", songDocument, null, Aspose.Html.Dom.XPath.XPathResultType.Any, null);
+
+        for (Node node; (node = songLyricsNode.IterateNext()) != null;)
+        {
+            Node line = node.FirstChild;
+            song.Lyrics.AppendLine(line.TextContent);
+
+            while (line.NextSibling != null)
+            {
+                line = line.NextSibling;
+                if (!string.IsNullOrWhiteSpace(line.TextContent))
+                {
+                    song.Lyrics.AppendLine(line.TextContent);
+                }
+            }
+
+            song.Lyrics.AppendLine();
+        }
+
+        if (song.Lyrics.Length < 10)
+        {
+            throw new Exception("Musica pequena....");
+        }
+
+        songLyricsNode = null;
+        songDocument = null;
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Erro {song.Name}. " + ex.Message);
+    }
+}
+
+//Number map
+Dictionary<string, int> numberMap = new();
+numberMap.Add("A Ceia do Senhor", 44);
+
+//Number match
+foreach (Song song in songList)
+{
+    if (numberMap.ContainsKey(song.Name))
+    {
+        song.Number = numberMap[song.Name];
+    }
+    else
+    {
+        Console.WriteLine($"Erro {song.Name}. SEM NÚMERO");
+    }
+}
+
+//Create files
+foreach (Song song in songList)
+{
+    using(StreamWriter sw = new StreamWriter($"C:\\source\\personal\\WebScrapping\\WebScrapping\\Result\\{song.Title}.txt", false))
+    {
+        sw.WriteLine(song.Lyrics);
+    }
+}
---
+++ WebScrapping/WebScrapping/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapping;
+public class Song
+{
+    public string? Name { get; set; } = "NNN";
+    public int Number { get; set; } = 0;
+    public string? Url { get; set; } = "UUU";
+    public StringBuilder Lyrics { get; set; } = new();
+    public string Title
+    {
+        get
+        {
+            return $"{string.Format("{0:D3}", Number)} - {Name}";
+        }
+    }
+    public override string ToString()
+    {
+        return $"{Number}: {Name} ({Lyrics?.Length} chars). {Url}";
+    }
+}
